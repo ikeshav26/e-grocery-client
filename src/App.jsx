b0f416e5f90f0029930c1f7712b0502622ff26e0
ikeshav26 <keshavgilhotra4@gmail.com>
2025-06-16 22:49:42 +0530

    Category section

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/products' element={<Products/>}/>
-          <Route path='/product/"id' element={<ProductDetails/>}/>
+          <Route path='/product/:id' element={<ProductDetails/>}/>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/my-orders' element={<Myorders/>}/>
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React, { useContext } from 'react'
 
 import { AppContext } from '../context/AppContext'
 import Hero from '../components/Hero'
+import Category from '../components/Category'
 
 const Home = () => {
 
   return (
     <div className='mt-10'>
       <Hero/>
+      <Category/>
     </div>
   )
 }
